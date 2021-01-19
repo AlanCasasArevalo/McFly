02d10272889a86eab261035403e978b196b712bd
Alan Casas <alancasasarevalo@gmail.com>
2021-01-19 16:06:37 +0100

    WIP: [FEATURE][NOTE CONTROLLER] * Green circle => note controller handle errors with try catch

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -2,8 +2,12 @@ const NoteModel = require("../model/note.model");
 
 const createNote = async (req, res, next) => {
     const body = req.body
-    const createModel = await NoteModel.create(body)
-    res.status(201).json(createModel)
+    try {
+        const createModel = await NoteModel.create(body)
+        res.status(201).json(createModel)
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = {
