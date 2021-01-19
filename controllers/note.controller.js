111a93b95b4415146d58def1c887b6d9ac59c481
Alan Casas <alancasasarevalo@gmail.com>
2021-01-19 16:04:17 +0100

    WIP: [TEST][NOTE CONTROLLER] * Green circle => note controller return 201 when note was created

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -3,6 +3,7 @@ const NoteModel = require("../model/note.model");
 const createNote = async (req, res, next) => {
     const body = req.body
     const createModel = await NoteModel.create(body)
+    res.status(201).json(createModel)
 }
 
 module.exports = {
