01d5d3dba2e0efd4f892005bd1a3334e0585b9f4
Alan Casas <alancasasarevalo@gmail.com>
2021-01-19 15:54:38 +0100

    WIP: [TEST][NOTE CONTROLLER] * Red circle => noteController should have createNote function to post new notes

--- test/unit/notes.controller.test.js
+++ test/unit/notes.controller.test.js
@@ -1,10 +1,29 @@
+const newNote = require('../mock-data/new-note.json')
+const httpsMocks = require('node-mocks-http')
+
 const noteController = require('../../controllers/note.controller')
+
+let req, res, next
+
+beforeEach(() => {
+    req = httpsMocks.createRequest()
+    res = httpsMocks.createResponse()
+    next = jest.fn()
+})
 
 describe('NoteController', () => {
     describe('NoteController POST new note', () => {
 
+        beforeEach(() => {
+            req.body = newNote
+        })
+
         it('It NoteController should be defined', () => {
             expect(noteController).toBeDefined()
+        })
+
+        it('It should have a createNote Function', () => {
+            expect(typeof noteController.createNote).toBe('function')
         })
 
     })
